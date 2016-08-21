b4e7fc79a922202c6198cc1f6ef75e75ddc72968
Ritik Mishra <ritik.mishra314@gmail.com>
2016-08-21 03:18:07 -0500

    reorganized and cleaned up markovtweet.js, minor changes to index.html

--- markovtweet.js
+++ markovtweet.js
@@ -1,94 +1,100 @@
-module.exports = function(username, callback){
-  //var nlp = require('.node_modules/nlp_compromise')
-  var twitter = require('twitter')
-  //var natural = require('.node_modules/natural')
-  var randobj = require('pick-pair')
-  var randitem = require('pick-item')
+//var nlp = require('.node_modules/nlp_compromise')
+var twitter = require('twitter')
+//var natural = require('.node_modules/natural')
+var randobj = require('pick-pair')
+var randitem = require('pick-item')
 
+//begining
+var tapi = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-  var args = process.argv.slice(2);
-  if(username){
-    if(username[0] == '@'){
-      username = username.substr(1);
-    }
-  }
+var tweets_text = []
+var mark_obj = {}
+var nouns = []
+var verbs = []
+var result = []
 
-  //begining
-
-  var tapi = new twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-
-  var tweets_text = []
-  var mark_obj = {}
-  var nouns = []
-  var verbs = []
-  var result = []
-  tapi.get('statuses/user_timeline', {screen_name: username, count: '200', include_rts: 'false'}, function(error, tweets, response){
-    if (error) return callback(error);
-
-    for(var i = 0; i < tweets.length; i++){
-      tweets_text[i] = tweets[i].text.split(" ")
-
-    }
-
-    for(var i = 0; i < tweets_text.length; i++){
-      for(var j = 0; j < (tweets_text[i].length - 2); j++){
-        if(mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]]){
+var markovchain = function (tweets_text, markov_data){
+  let mark_obj = markov_data
+  for(var i = 0; i < tweets_text.length; i++){
+    //loops through tweet-arrays split into words
+    for(var j = 0; j < (tweets_text[i].length - 2); j++){
+      //loops through words in said tweet arrays
+      if(mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]]){//if the key exists
+        if(mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]].indexOf(tweets_text[i][j+2]) == -1){
+          //console.log(mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]])
           mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]].push(tweets_text[i][j+2])
-
         }
-        else{
-          mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]] = [tweets_text[i][j+2]]
-
-        }
-  /*
-        if(nlp.text(tweets_text[i][j]).sentences[0].terms[0].pos["Noun"]){
-          nouns.push(tweets_text[i][j])
-        }
-
-        else if(nlp.text(tweets_text[i][j]).sentences[0].terms[0].pos["Verb"]){
-          verbs.push(tweets_text[i][j])
-        }
-  */
-      }
-    }
-
-
-    while(true){
-      if(result.length == 0){
-        filler=randobj(mark_obj)
-
-        filler2=Object.keys(filler)[0].split(" ")
-        result.push(filler2[0])
-        result.push(filler2[1])
-        result.push(randitem(filler[Object.keys(filler)[0]]))
-      }
-      else if(result[result.length-1] == undefined){
-        result.pop()
-        break;
       }
       else{
-        result.push(randitem( mark_obj[result[result.length-2] + " " + result[result.length-1]] ))
+        mark_obj[tweets_text[i][j] + " " + tweets_text[i][j + 1]] = [tweets_text[i][j+2]]
+        //console.log(tweets_text[i][j+2])
       }
     }
-    /*
-    var result_nlp = nlp.text(result.join(" "))
-    if (!(result_nlp.sentences[0].terms[0].pos["Noun"])){
-      result.unshift(randitem(nouns))
-    }
-    */
-    //console.log(result.join(" "))
-    return callback(null, result.join(" "))
+  }
+  console.log("Markov chained")
+  return mark_obj
 
-
-
-
-
-
-
-  });// end of tapi.get('statuses/user_timeline' . . .
 }
+
+var markov_to_tweet = function (mark_obj) {
+  let result = []
+  while(true){
+    if(result.length == 0){
+      filler=randobj(mark_obj)
+      filler2=Object.keys(filler)[0].split(" ")
+      result.push(filler2[0])
+      result.push(filler2[1])
+      result.push(randitem(filler[Object.keys(filler)[0]]))
+    }
+    else if(result[result.length-1] == undefined){
+      result.pop()
+      break;
+    }
+    else{
+      result.push(randitem(mark_obj[result[result.length-2] + " " + result[result.length-1]] ))
+    }
+  }
+  return result
+}
+//single  user markovtweet
+// usernames is a list of strings, callback is a function with args (err, data), inceptions is an int
+var markovtweet = function (usernames, inceptions, callback){
+  //gather tweets from users
+  for(var i = 0; i < usernames.length; i++){
+    if(usernames[i][0] == '@'){
+      usernames[i] = usernames[i].substr(1);
+    }
+    tapi.get('statuses/user_timeline', {screen_name: usernames[i], count: '200', include_rts: 'false'}, function(error, tweets, response){
+      if (error) return callback(error);
+
+      for(var i = 0; i < tweets.length; i++){
+        tweets_text.push(tweets[i].text.split(" "))
+      }
+      mark_obj = markovchain(tweets_text, mark_obj)
+      console.log("Tweets obtained")
+
+      console.log("Proceeding into inception for loop")
+      for(var i = 0; i < inceptions; i++){
+        let fake_tweets_text = []
+        for(var i = 0; i < 50; i++){
+          fake_tweets_text.push(markov_to_tweet(mark_obj))
+        }
+        mark_obj = markovchain(fake_tweets_text, mark_obj)
+      }
+
+      result = markov_to_tweet(mark_obj)
+      return callback(null, result.join(" "))
+    })
+  }
+
+
+}// end of function
+
+
+
+module.exports = markovtweet
