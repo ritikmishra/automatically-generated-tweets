a16ba5310ae633b9714347570a0b679d9985bd72
Ritik Mishra <ritik.mishra314@gmail.com>
2016-08-24 23:26:05 -0500

    website now functional

---
+++ index.js
@@ -0,0 +1,27 @@
+var httpGetAsync = function (theUrl, callback){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(null, xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+var httpGetCallback = function(error, tweet){
+	if(error){console.log(error);}
+	document.getElementById("result").innerHTML = tweet;
+
+}
+
+var btnclick = function(){
+  var usernames = document.getElementById('username').value.split(" ");
+  var url = 'http://localhost:5000/tweet?'
+  for(var i = 0; i < usernames.length; i++){
+    url = url + "usernames=" + usernames[i] + "&"
+  }
+  httpGetAsync(url,function(error, tweet){
+    if(error){console.log(error);}
+    document.getElementById('result').innerHTML = tweet;
+  } )
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var dispatcher = require('httpdispatcher');
 var markovtweet = require('./markovtweet');
-
+var fs = require('fs')
 //We need a function which handles requests and send response
 //Lets use our dispatcher
 function handleRequest(request, response){
@@ -22,11 +22,11 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(process.env.PORT || 5000, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", process.env.PORT);
+    console.log("Server listening on: http://localhost:%s", process.env.PORT || 5000);
 });
 
 //A sample GET request
-dispatcher.onGet("/", function(req, res) {
+dispatcher.onGet("/tweet", function(req, res) {
   console.log(req.params)
   if(req.params.usernames){
     markovtweet(req.params.usernames, function(err, data){
@@ -45,4 +45,35 @@ dispatcher.onGet("/", function(req, res) {
     })
   }
 
+});
+
+//A sample GET request
+dispatcher.onGet("/", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var file = fs.readFile('index.html', 'utf8', function callback(err, data){
+    if (err) return console.error(err);
+    var html = data
+    res.write(html)
+    res.end();
+  });
+});
+
+dispatcher.onGet("/index.js", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/javascript'});
+  var file = fs.readFile('index.js', 'utf8', function callback(err, data){
+    if (err) return console.error(err);
+    var html = data
+    res.write(html)
+    res.end();
+  });
+});
+
+dispatcher.onGet("/index.css", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/css'});
+  var file = fs.readFile('index.css', 'utf8', function callback(err, data){
+    if (err) return console.error(err);
+    var html = data
+    res.write(html)
+    res.end();
+  });
 });
